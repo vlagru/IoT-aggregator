29ee2e00025e4ccc57121a860bea29bae132a8ac
vlagru <v.grulich@seznam.cz>
2022-04-17 12:45:55 +0200

    Initial commit

---
+++ src/test/java/com/cleverlance/academy/aggregator/IosServerAggregatorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cleverlance.academy.aggregator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IosServerAggregatorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
