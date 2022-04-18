c70a733d98faaf60e0c55e23152f1acf15811497
vlagru <v.grulich@seznam.cz>
2022-04-18 16:35:01 +0200

    Add test with MVC class testing get and post methods.

--- src/test/java/com/cleverlance/academy/aggregator/controller/AggregatorControllerTest.java
+++ src/test/java/com/cleverlance/academy/aggregator/controller/AggregatorControllerTest.java
@@ -5,24 +5,50 @@ import com.cleverlance.academy.aggregator.model.Identification;
 import com.cleverlance.academy.aggregator.model.Person;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.web.servlet.MockMvc;
 
-import java.util.List;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 
 @SpringBootTest
+@AutoConfigureMockMvc
 public class AggregatorControllerTest {
 
+    @Autowired
+    public MockMvc mockMvc;
+
     @Test
-    void saveServerAndGetIdentificationsTest(){
-        AggregatorController controller = new AggregatorController();
+    void testWithMock() throws Exception {
 
-        controller.saveServer(new Identification("Moje meteostanice",
-                new Address("Horova", 31, "Praha"),
-                new Person("Alex", "Novy")));
-        List<Identification> list = controller.getIdentifications().getBody();
+        //TEST POST
+        this.mockMvc.perform(post("/server")
+                        .contentType("application/json").
+                        content("{\"name\": \"aaaa\"}"))
+                .andExpect(status().isOk());
 
-        Assertions.assertNotNull(list);
-        Assertions.assertEquals(1, list.size());
+        //TEST GET
+        this.mockMvc.perform(get("/server")).andDo(print()).andExpect(status().isOk());
+
     }
+
+
+//    @Test
+//    void saveServerAndGetIdentificationsTest(){
+//        AggregatorController controller = new AggregatorController();//nemelo by se takto ukladat, pouziju MVC namist
+//
+//        controller.saveServer(new Identification("Moje meteostanice",
+//                new Address("Horova", 31, "Praha"),
+//                new Person("Alex", "Novy")));
+//        List<Identification> list = controller.getIdentifications().getBody();
+//
+//        Assertions.assertNotNull(list);
+//        Assertions.assertEquals(1, list.size());
+//    }
 
 }
