0a1517db3822aebf51465a62c230291f0ae82796
vlagru <v.grulich@seznam.cz>
2022-04-18 14:26:29 +0200

    Heroku deploy.

--- src/main/java/com/cleverlance/academy/aggregator/controller/AggregatorController.java
+++ src/main/java/com/cleverlance/academy/aggregator/controller/AggregatorController.java
@@ -43,6 +43,7 @@ public class AggregatorController {
     /**
      * post pro ulozeni na server
      * header: content-type application/json
+     * restman nebo postman
      */
 
     @PostMapping("/server")
